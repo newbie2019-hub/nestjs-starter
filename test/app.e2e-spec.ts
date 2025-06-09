747ff79e90def8624eb6431069ded83e94bdde13
Yvan Sabay <sabayyvan2018@gmail.com>
2025-06-09 14:29:22 +0800

    added separate setup for testing

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,4 +1,5 @@
 import * as request from 'supertest';
+import { server } from './setup';
 
 describe('AppController (e2e)', () => {
   it('/ (GET)', async () => {
--- test/setup.ts
+++ test/setup.ts
@@ -1,6 +1,5 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
-import * as request from 'supertest';
 import { App } from 'supertest/types';
 import { AppModule } from './../src/app.module';
 import helmet from 'helmet';
@@ -9,74 +8,43 @@ import { DatabaseService } from 'src/database/database.service';
 import { CacheService } from 'src/core/cache/cache.service';
 // import { ConfigService } from '@nestjs/config';
 
-describe('AppController (e2e)', () => {
-  let app: INestApplication<App>;
-  let server: App;
-  // let configService: ConfigService;
-  let databaseService: DatabaseService;
-  let cacheService: CacheService;
+let app: INestApplication<App>;
+let server: App;
+// let configService: ConfigService;
+let databaseService: DatabaseService;
+let cacheService: CacheService;
 
-  beforeEach(async () => {
-    const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [AppModule],
-    }).compile();
+beforeEach(async () => {
+  const moduleFixture: TestingModule = await Test.createTestingModule({
+    imports: [AppModule],
+  }).compile();
 
-    app = moduleFixture.createNestApplication();
-    // Copy what you have in main.ts to test the real app
-    app.use(helmet());
-    app.useGlobalPipes(
-      new CustomValidationPipe({
-        whitelist: true,
-      }),
-    );
+  app = moduleFixture.createNestApplication();
+  // Copy what you have in main.ts to test the real app
+  app.use(helmet());
+  app.useGlobalPipes(
+    new CustomValidationPipe({
+      whitelist: true,
+    }),
+  );
 
-    await app.init();
+  await app.init();
 
-    // Retrieving config service
-    // configService = app.get(ConfigService);
-    databaseService = app.get(DatabaseService);
-    cacheService = app.get(CacheService);
-    server = app.getHttpServer();
-  });
-
-  it('/ (GET)', async () => {
-    // const env = configService.get(`environment`) as string;
-    // console.log(`ENVIRONMENT: ${env}`);
-    await databaseService.user.create({
-      data: {
-        email: 'sample@gmail.com',
-        name: 'Michael Jordan',
-      },
-    });
-
-    return request(server)
-      .get('/')
-      .expect(200)
-      .expect(({ body }: { body: { data: any } }) => {
-        expect(body.data).toBe('Hello World');
-      });
-  });
-
-  // // 2nd Test
-  // it('/ (GET)', async () => {
-  //   const users = await databaseService.user.count();
-  //   console.log(`1st Test Count, expecting 0: got ${users}`);
-
-  //   return request(server)
-  //     .get('/')
-  //     .expect(200)
-  //     .expect(({ body }: { body: { data: any } }) => {
-  //       expect(body.data).toBe('Hello World');
-  //     });
-  // });
-
-  afterAll(async () => {
-    await app.close();
-  });
-
-  // Reset database and cache
-  afterEach(async () => {
-    await cacheService.reset();
-    await databaseService.reset();
-  });
+  // Retrieving config service
+  // configService = app.get(ConfigService);
+  databaseService = app.get(DatabaseService);
+  cacheService = app.get(CacheService);
+  server = app.getHttpServer();
 });
+
+afterAll(async () => {
+  await app.close();
+});
+
+// Reset database and cache
+afterEach(async () => {
+  await cacheService.reset();
+  await databaseService.reset();
+});
+
+export { server };
